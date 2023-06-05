ff5cb24b8080a50979a0aae6b1d82ef7a3016a28
Hjalte Albergson <123952556+HAlbergson@users.noreply.github.com>
2023-06-05 14:49:44 +0200

    done

--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -6,49 +6,58 @@ window.addEventListener("load", initApp);
 const animals = [];
 
 function initApp() {
-  console.log("Hi World");
-  createAnimal("Bonobo", "monkey", 420);
-  console.log(animals);
-  document.querySelector("#create-form").addEventListener("submit", createFormSubmit);
+  createAnimal("Bo", "abe", 22); // test af del 1
+  console.log(animals); // test af del 1
+  // event på formular
+  document.querySelector("#create-form").addEventListener("submit", createFormSubmitted);
 }
 
 function createAnimal(name, type, age) {
+  // nyt animal objekt med værdier fra argumenter
   const animal = {
     name: name,
     type: type,
     age: age,
   };
-  animals.push(animal);
+  animals.push(animal); // det nye objekt tilføjes den globale liste, animals
+  console.log(animal);
   return animal;
 }
 
-function createFormSubmit(event) {
-  event.preventDefault();
+function createFormSubmitted(event) {
+  event.preventDefault(); // prevent default submit event
 
-  const form = event.target;
-
+  const form = event.target; // reference til formularen
+  // få fat i værdier fra formular
   const name = form.name.value;
   const type = form.type.value;
   const age = form.age.value;
-
+  // kald createAnimal med værdier fra formular
   createAnimal(name, type, age);
-  console.log(animals);
+  console.log(animals); // test animals
+  // sørg for at alle animals vises (+ det nye) ved at kalde showAnimals
   showAnimals();
 }
 
 function showAnimals() {
+  // sort animals by name
   animals.sort(compareName);
+  console.log(animals);
+  // reset tbody
   document.querySelector("#list-container tbody").innerHTML = "";
+
   for (const animal of animals) {
     const html = /*html*/ `
             <tr>
-            <td>${animal.name}</td>
-            <td>${animal.type}</td>
-            <td>${animal.age}</td>
-            </tr>`;
+                <td>${animal.name}</td>
+                <td>${animal.type}</td>
+                <td>${animal.age}</td>
+            </tr>
+        `;
     document.querySelector("#list-container tbody").insertAdjacentHTML("beforeend", html);
   }
 }
+
 function compareName(a, b) {
-  return a.name.localCompare(b.name);
+  return a.name.localeCompare(b.name);
 }
