25a259faded796b4ebe2c01b00a0da72d6ba77e9
Hjalte Albergson <123952556+HAlbergson@users.noreply.github.com>
2023-06-05 13:09:40 +0200

    Update script.js

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,27 @@
 "use strict";
 
+window.addEventListener("load", initApp);
+
+let users = [];
+
+async function initApp() {
+  users = await getUser();
+  console.log(users);
+  createUser();
+}
+
+async function getUser() {
+  const response = await fetch("users.json");
+  const data = response.json();
+  return data;
+}
+
+function createUser() {
+  let html = "";
+  for (const user of users) {
+    html += /*html*/ `
+    <li>${user.name} - ${user.active} - ${user.role}</li>
+    `;
+  }
+  document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+}
