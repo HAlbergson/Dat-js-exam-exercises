9a7b7e6e8d7d8335bdf9fd9899dc7e3bdfa6503e
Hjalte Albergson <123952556+HAlbergson@users.noreply.github.com>
2023-06-05 14:31:52 +0200

    nye ting

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,27 +1,70 @@
-"use strict";
+// ----------------------------------------- exercise 01 -----------------------------------
+// "use strict";
 
-window.addEventListener("load", initApp);
+// window.addEventListener("load", initApp);
 
-let users = [];
+// let users = [];
 
-async function initApp() {
-  users = await getUser();
-  console.log(users);
-  createUser();
-}
+// async function initApp() {
+//   users = await getUser();
+//   console.log(users);
+//   createUser(users);
+//   displayAdmins(users);
+// }
+// async function getUser() {
+//   const response = await fetch("users.json");
+//   const data = response.json();
+//   return data;
+// }
 
-async function getUser() {
-  const response = await fetch("users.json");
-  const data = response.json();
-  return data;
-}
+// function createUser(listOfUsers) {
+//   for (const user of listOfUsers) {
+//     const html = /*html*/ `
+//     <li>${user.name} - ${user.active} - ${user.role}</li>
+//     `;
+//     document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+//   }
+// }
+// function displayAdmins(users) {
+//   const admins = users.filter((user) => user.role === "admin");
+//   document.querySelector("#admin-count").textContent = admins.length;
+// }
+// ------------------------------------------- exercise 02 --------------------------------------------
+// "use strict";
 
-function createUser() {
-  let html = "";
-  for (const user of users) {
-    html += /*html*/ `
-    <li>${user.name} - ${user.active} - ${user.role}</li>
-    `;
-  }
-  document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
-}
+// window.addEventListener("load", initApp);
+// let users = [];
+// async function initApp() {
+//   users = await getUser();
+//   console.log("hi world");
+
+//   createUserList(users);
+//   displayRoleCount(users);
+// }
+
+// async function getUser() {
+//   const response = await fetch("users.json");
+//   const data = response.json();
+//   console.log(data);
+//   return data;
+// }
+
+// function createUserList(listOfUsers) {
+//   for (const user of listOfUsers) {
+//     const html = /*html*/ `
+//     <li>${user.name} - ${user.active} - ${user.role}</li>`;
+//     document.querySelector("#userlist").insertAdjacentHTML("beforeend", html);
+//   }
+// }
+
+// function displayRoleCount(users) {
+//   const adminCount = users.filter((user) => user.role === "admin");
+//   console.log(adminCount);
+//   document.querySelector("#admin-count").textContent = adminCount.length;
+//   const userCount = users.filter((user) => user.role === "user");
+//   console.log(userCount);
+//   document.querySelector("#user-count").textContent = userCount.length;
+//   const guestCount = users.filter((user) => user.role === "guest");
+//   console.log(guestCount);
+//   document.querySelector("#guest-count").textContent = guestCount.length;
+// }
--- environments/environment-02/script.js
+++ environments/environment-02/script.js
@@ -1,2 +1,54 @@
+// ------------------------------------------ exercise 03 -----------------------------
 "use strict";
 
+window.addEventListener("load", initApp);
+
+const animals = [];
+
+function initApp() {
+  console.log("Hi World");
+  createAnimal("Bonobo", "monkey", 420);
+  console.log(animals);
+  document.querySelector("#create-form").addEventListener("submit", createFormSubmit);
+}
+
+function createAnimal(name, type, age) {
+  const animal = {
+    name: name,
+    type: type,
+    age: age,
+  };
+  animals.push(animal);
+  return animal;
+}
+
+function createFormSubmit(event) {
+  event.preventDefault();
+
+  const form = event.target;
+
+  const name = form.name.value;
+  const type = form.type.value;
+  const age = form.age.value;
+
+  createAnimal(name, type, age);
+  console.log(animals);
+  showAnimals();
+}
+
+function showAnimals() {
+  animals.sort(compareName);
+  document.querySelector("#list-container tbody").innerHTML = "";
+  for (const animal of animals) {
+    const html = /*html*/ `
+            <tr>
+            <td>${animal.name}</td>
+            <td>${animal.type}</td>
+            <td>${animal.age}</td>
+            </tr>`;
+    document.querySelector("#list-container tbody").insertAdjacentHTML("beforeend", html);
+  }
+}
+function compareName(a, b) {
+  return a.name.localCompare(b.name);
+}
