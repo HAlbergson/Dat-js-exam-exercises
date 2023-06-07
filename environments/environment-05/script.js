1f32cb4f0c39d9b037d376078dda4202df0aa7df
Hjalte Albergson <123952556+HAlbergson@users.noreply.github.com>
2023-06-07 22:48:57 +0200

    yeppa

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,47 @@
-"use strict";
+// ----------------------------------------------------------- Exercise 11 -----------------------------------------
+
+// import { courses } from "./courses.js";
+
+// window.addEventListener("load", initApp);
+
+// function initApp() {
+//   //   showCourses(courses);
+//   createNewCourse("hjalte er bedst", 10, "Hjalte");
+// }
+
+// function showCourses(courses) {
+//   for (const course of courses) {
+//     const html = /*html*/ `
+//         <li>${course.name} ${course.ectsPoints} ${course.teacher}</li>`;
+//     document.querySelector("#courses-list").insertAdjacentHTML("beforeend", html);
+//   }
+// }
+
+// function createNewCourse(name, ectsPoints, teacher) {
+//   const newCourse = { name: name, ectsPoints: ectsPoints, teacher: teacher };
+//   courses.push(newCourse);
+//   showCourses(courses);
+// }
+
+// ------------------------------------------------------ Exercise 12 ---------------------------------------------
+
+// import { courses } from "./courses.js";
+
+// window.addEventListener("load", initApp);
+
+// function initApp() {
+//   showCourses(courses);
+// }
+
+// function showCourses(courses) {
+//   sortByEctsPoints();
+//   for (const course of courses) {
+//     const html = /*html*/ `
+//         <li>${course.name} ${course.ectsPoints}</li>`;
+//     document.querySelector("#courses-list").insertAdjacentHTML("beforeend", html);
+//   }
+// }
+
+// function sortByEctsPoints() {
+//   courses.sort((a, b) => a.ectsPoints - b.ectsPoints);
+// }
