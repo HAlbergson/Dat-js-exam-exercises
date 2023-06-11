c0244f72443cb3ae23ada5455e84349e87823981
HAlbergson <hjalte3480@gmail.com>
2023-06-11 17:17:57 +0200

    jhgfd

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,2 +1,20 @@
 "use strict";
 
+window.addEventListener("load", initApp);
+
+let students = [];
+
+function initApp() {
+  console.log("script is running");
+  showStudents();
+}
+
+function showStudents(name, email, age) {
+  const name = document.getElementById("name").value;
+  //   const name = document.querySelector("#text").value;
+  //   const email = document.querySelector("#email").value;
+  //   const age = document.querySelector("#age").value;
+  //   const name = studnet.name;
+  //   const email = studnet.email;
+  //   const age = studnet.age;
+}
